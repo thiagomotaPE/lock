dcbf110f9579c18e3eab57b2071195155fa004c0
Mota <thiagomotape@hotmai.com.br>
2025-12-07 14:14:26 -0300

    Criando validação de senha para novos usuarios

--- server/lock/src/main/java/com/api/lock/user/service/UserService.java
+++ server/lock/src/main/java/com/api/lock/user/service/UserService.java
@@ -14,6 +14,7 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    //Busca todos os usuarios
     public ResponseEntity<List<User>> getAllUsers() {
         try {
             var allUsers = userRepository.findAll();
@@ -23,13 +24,34 @@ public class UserService {
         }
     }
 
+    //Cria um novo usuario
     public ResponseEntity<List<User>> RegisterNewUser(CreateUserDto createUserDto) {
+        verifyPassword(createUserDto.password());
         try {
             User newUser = new User(createUserDto);
             userRepository.save(newUser);
             return ResponseEntity.ok().build();
         } catch (Exception e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    //Verifica se a senha nova tem o padrão necessario
+    public void verifyPassword(String newPassword) {
+        if (newPassword.length() < 8)
+            throw new IllegalArgumentException("A senha deve ter pelo menos 8 caracteres");
+
+        if (!newPassword.matches(".*[A-Z].*"))
+            throw new IllegalArgumentException("A senha deve conter pelo menos 1 letra maiúscula");
+
+        if (!newPassword.matches(".*[a-z].*"))
+            throw new IllegalArgumentException("A senha deve conter pelo menos 1 letra minúscula");
+
+        if (!newPassword.matches(".*\\d.*"))
+            throw new IllegalArgumentException("A senha deve conter pelo menos 1 número");
+
+        if (!newPassword.matches(".*[^a-zA-Z0-9].*")) {
+            throw new IllegalArgumentException("A senha deve conter pelo menos 1 caractere especial");
         }
     }
 }
